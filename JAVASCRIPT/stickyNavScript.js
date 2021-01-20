736c8b9088dcaa5145466d9adcaa3a5def1f4a41
Joseph Fitzgerald <jefitzge@users.noreply.github.com>
2021-01-20 15:31:24 -0500

    Testing NavBar solution
    -Changed navigation bar script based on another users input

--- JAVASCRIPT/stickyNavScript.js
+++ JAVASCRIPT/stickyNavScript.js
@@ -1,11 +1,22 @@
-window.onscroll = function() {stickyNavBar()}; 
-var navBar = document.getElementById("mynavBar")
-var sticky = navBar.offsetTop;
+// window.onscroll = function() {stickyNavBar()}; 
+// var navBar = document.getElementById("mynavBar")
+// var sticky = navBar.offsetTop;
 
-function stickyNavBar(){
-	if(window.pageYOffset > sticky){
-		navBar.classList.add("sticky");
-	} else{
-		navBar.classList.remove("sticky");
+// function stickyNavBar(){
+// 	if(window.pageYOffset > sticky){
+// 		navBar.classList.add("sticky");
+// 	} else{
+// 		navBar.classList.remove("sticky");
+// 	}
+// }
+
+const nav = document.getElementById('#mynavBar')
+const firstScreen = document.querySelector('.top-container')
+document.addEventListener('scroll', function(e) {
+	let scrollCount = self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop)
+	if (scrollCount > firstScreen.clientHeight) {
+		nav.classList.add('sticky')
+	} else {
+		nav.classList.remove('sticky')
 	}
-}
+})
