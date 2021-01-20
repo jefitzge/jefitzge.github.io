fb59c058ec1abdc85db117525eb7ac6729b4c02f
Joseph Fitzgerald <jefitzge@users.noreply.github.com>
2021-01-20 16:05:24 -0500

    Update stickyNavScript.js

--- JAVASCRIPT/stickyNavScript.js
+++ JAVASCRIPT/stickyNavScript.js
@@ -10,13 +10,14 @@
 // 	}
 // }
 
-const nav = document.getElementById('#mynavBar')
+const nav = document.querySelector('#mynavBar')
 const firstScreen = document.querySelector('.top-container')
 document.addEventListener('scroll', function(e) {
-	let scrollCount = self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop)
-	if (scrollCount > firstScreen.clientHeight) {
-		nav.classList.add('sticky')
-	} else {
-		nav.classList.remove('sticky')
-	}
-})
+  let scrollCount = self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop)
+
+  if (scrollCount > firstScreen.clientHeight) {
+    nav.classList.add('sticky')
+  } else {
+    nav.classList.remove('sticky')    
+  }
+}) 
