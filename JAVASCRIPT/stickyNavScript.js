75c1a9fc36d11f5cbb5b943e506a47c1e59344f5
Joseph Fitzgerald <jefitzge@users.noreply.github.com>
2021-01-27 12:27:44 -0500

    Trying new sticky nav solution

--- JAVASCRIPT/stickyNavScript.js
+++ JAVASCRIPT/stickyNavScript.js
@@ -10,14 +10,34 @@
 // 	}
 // }
 
-const nav = document.querySelector('#mynavBar')
-const firstScreen = document.querySelector('.top-container')
-document.addEventListener('scroll', function(e) {
-  let scrollCount = self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop);
+// const nav = document.querySelector('#mynavBar')
+// const firstScreen = document.querySelector('.top-container')
+// document.addEventListener('scroll', function(e) {
+//   let scrollCount = self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop);
 
-  if (scrollCount > firstScreen.clientHeight) {
-    nav.classList.add('sticky')
-  } else {
-    nav.classList.remove('sticky')    
-  }
-}) 
+//   if (scrollCount > firstScreen.clientHeight) {
+//     nav.classList.add('sticky')
+//   } else {
+//     nav.classList.remove('sticky')    
+//   }
+// }) 
+
+$(document).ready(function() {
+  var stickyNavTop = $('.nav').offset().top;
+
+  var stickyNav = function(){
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > stickyNavTop) { 
+      $('.nav').addClass('sticky');
+    } else {
+      $('.nav').removeClass('sticky'); 
+    }
+  };
+
+  stickyNav();
+
+  $(window).scroll(function() {
+    stickyNav();
+  });
+});
