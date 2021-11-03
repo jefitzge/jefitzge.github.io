d934b10f3655d560b8d31d4993eb9b1abaab32f5
Joseph Fitzgerald <26047215+jefitzge@users.noreply.github.com>
2021-11-02 22:09:58 -0400

    New Scroll To Top Button and footer animation
    -Added scroll to top for better page navigation
    -Added new hover animation to footer icons

---
+++ JAVASCRIPT/scrollToTop.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function(){
+    var scroll =document.querySelector('.scrollTop');
+    scroll.classList.toggle("active", window.scrollY > 500);
+})
+function scrollToTop(){
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+}
